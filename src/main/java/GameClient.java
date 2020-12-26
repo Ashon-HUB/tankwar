b2b3a79e30aa5ed9f24e7bd4500695d6b785c944
Ashon <asdzxc99911@gmail.com>
2020-12-26 13:22:44 +0800

    製作Wall

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,98 +1,113 @@
 import object.Direction;
 import object.Tank;
+import object.Wall;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class GameClient extends JComponent {
-        private int width;
-        private int height;
-        Tank Play ;
+    private int width;
+    private int height;
+    Tank Play;
+    private ArrayList<Tank> enemyTanks = new ArrayList<>();
+    private ArrayList<Wall> walls = new ArrayList<>();
 
 
 
-        GameClient(){
-            this(800,600);
-        }
-        public GameClient(int width,int height){
-            this.width = width;
-            this.height = height;
-            this.setPreferredSize(new Dimension(width,height));
-            init();
+    GameClient() {
+        this(800, 600);
+    }
 
-            new Thread(()->{
-                while (true){
-                    repaint();
-                    try{
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+    public GameClient(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.setPreferredSize(new Dimension(width, height));
+        init();
+
+        new Thread(() -> {
+            while (true) {
+                repaint();
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }).start();
-        }
-
-        public void init(){
-            Play = new Tank(100,100, Direction.DOWN);
-    }
-
-    public void keyPressed(KeyEvent e){
-            boolean[] dirs = Play.getDirs();
-
-            switch (e.getKeyCode()){
-                case KeyEvent.VK_UP:
-                    dirs[0]=true;
-//                    Play.setDirection(Direction.UP);
-//                    Play.setY(Play.getY()-5);
-                    break;
-                case KeyEvent.VK_DOWN:
-                    dirs[1]=true;
-//                    Play.setDirection(Direction.DOWN);
-//                    Play.setY(Play.getY()+5);
-                    break;
-                case KeyEvent.VK_LEFT:
-                    dirs[2]=true;
-//                    Play.setDirection(Direction.RIGHT);
-//                    Play.setX(Play.getX()+5);
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    dirs[3]=true;
-//                    Play.setDirection(Direction.LEFT);
-//                    Play.setX(Play.getX()-5);
-                    break;
-
             }
-            //repaint();
-        Play.move();
+        }).start();
     }
-    public void keyReleased(KeyEvent e){
+
+    public void init() {
+        Play = new Tank(375, 100, Direction.DOWN);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                enemyTanks.add(new Tank(250 + j * 80, 300 + 80 * i, Direction.UP,true));
+            }
+        }
+        walls.add(new Wall(100,200,false,10));
+        walls.add(new Wall(200,200,true,13));
+        walls.add(new Wall(650,200,false,10));
+    }
+
+
+    public void keyPressed(KeyEvent e) {
         boolean[] dirs = Play.getDirs();
 
-        switch (e.getKeyCode()){
+        switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
-                dirs[0]=false;
+                dirs[0] = true;
 //                    Play.setDirection(Direction.UP);
 //                    Play.setY(Play.getY()-5);
                 break;
             case KeyEvent.VK_DOWN:
-                dirs[1]=false;
+                dirs[1] = true;
 //                    Play.setDirection(Direction.DOWN);
 //                    Play.setY(Play.getY()+5);
                 break;
             case KeyEvent.VK_LEFT:
-                dirs[2]=false;
+                dirs[2] = true;
 //                    Play.setDirection(Direction.RIGHT);
 //                    Play.setX(Play.getX()+5);
                 break;
             case KeyEvent.VK_RIGHT:
-                dirs[3]=false;
+                dirs[3] = true;
 //                    Play.setDirection(Direction.LEFT);
 //                    Play.setX(Play.getX()-5);
                 break;
 
         }
-        Play.move();
+        //repaint();
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+        boolean[] dirs = Play.getDirs();
+
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                dirs[0] = false;
+//                    Play.setDirection(Direction.UP);
+//                    Play.setY(Play.getY()-5);
+                break;
+            case KeyEvent.VK_DOWN:
+                dirs[1] = false;
+//                    Play.setDirection(Direction.DOWN);
+//                    Play.setY(Play.getY()+5);
+                break;
+            case KeyEvent.VK_LEFT:
+                dirs[2] = false;
+//                    Play.setDirection(Direction.RIGHT);
+//                    Play.setX(Play.getX()+5);
+                break;
+            case KeyEvent.VK_RIGHT:
+                dirs[3] = false;
+//                    Play.setDirection(Direction.LEFT);
+//                    Play.setX(Play.getX()-5);
+                break;
+
+        }
+
         //repaint();
     }
 
@@ -101,9 +116,15 @@ public class GameClient extends JComponent {
         super.paintComponent(g);
 
         g.setColor(Color.BLACK);
-        g.fillRect(0,0,width,height);
+        g.fillRect(0, 0, width, height);
 
         Play.draw(g);
+        for (Tank tank : enemyTanks) {
+            tank.draw(g);
+        }
+        for(Wall wall : walls){
+            wall.draw(g);
+        }
 
 //        g.drawImage(Play.getImage(),Play.getX(),Play.getY(),null);
     }
